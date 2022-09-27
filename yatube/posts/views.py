48aca464938c482e564010cdc6121c11062c1d6a
Кирилл Астраханцев <picnichek1996@yandex.ru>
2022-09-27 10:18:27 +0500

    latest

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
-    text = 'Это главная страница проекта Yatube'
-    context = {'text': text}
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'posts': posts}
     return render(request, template, context)
 
+
 def group_posts(request, slug):
-    #return HttpResponse(f'посты со значением {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
-    context = {'text': text}
+    context = {'group': group, 'posts': posts, }
     return render(request, template, context)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates') # добавил шаблоны
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')  # добавил шаблоны
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
